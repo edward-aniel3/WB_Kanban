1f1cebfb3caff1168b2df032850e4a1d567811bc
edward-aniel3 <edwarddanieleviri@gmail.com>
2026-08-19 22:25:45 +0800

    frontend init

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+ return (
+    <div className="bg-primary text-white min-h-screen flex items-center justify-center">
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
